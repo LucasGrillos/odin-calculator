930d66e214e635365181b911f4302800a759ceb4
Lucas Grillos <LucasGrillos@gmail.com>
2022-08-19 20:31:12 -0700

    Add basic logic for math operations

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 window.addEventListener('load', function() {
     
 }, false);
+
+const addition = (num1, num2) => {
+    return num1+num2;
+}
+
+const subtraction = (num1, num2) => {
+    return num1-num2;
+}
+
+const multiplication = (num1, num2) => {
+    return num1*num2;
+}
+
+const division = (num1, num2) => {
+    return num1/num2;
+}
+
+const operate = (num1, num2, operation) => {
+    if (operation === '+') {
+        addition(num1, num2);
+    }
+
+    else if (operation === '-') {
+        subtraction(num1, num2);
+    }
+
+    else if (operation === '*') {
+        multiplication(num1, num2);
+    }
+
+    else if (operation == '/') {
+        division = (num1, num2);
+    }
+}
